bca0e181dd866b7272953e7e05a0b310f56b9704
yijun <3220304324@qq.com>
2025-08-16 22:31:04 +0800

    build(service-util): 添加 knife4j 依赖

---
+++ service-oa/src/main/java/org/example/service/SysRoleService.java
@@ -0,0 +1,10 @@
+package org.example.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.example.model.system.SysRole;
+
+import java.util.List;
+
+public interface SysRoleService extends IService<SysRole> {
+
+}
---
+++ service-oa/src/main/java/org/example/service/impl/SysRoleServiceImpl.java
@@ -0,0 +1,19 @@
+package org.example.service.impl;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.example.mapper.SysRoleMapper;
+import org.example.model.system.SysRole;
+import org.example.service.SysRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SysRoleServiceImpl extends ServiceImpl<SysRoleMapper, SysRole> implements SysRoleService {
+
+    @Autowired
+    private SysRoleMapper sysRoleMapper;
+}
