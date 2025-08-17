638be4a768512048ef4128bf240c298a094342cb
yijun <3220304324@qq.com>
2025-08-17 15:34:46 +0800

    feat(common): 添加全局异常处理和自定义异常类

---
+++ common/service-util/src/main/java/org/example/handle/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package org.example.handle;
+
+import org.example.exception.YiJunException;
+import org.example.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * 全局异常处理类
+ *
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail();
+    }
+
+    @ExceptionHandler(ArithmeticException.class)
+    @ResponseBody
+    public Result error(ArithmeticException e){
+        e.printStackTrace();
+        return Result.fail().message("执行了特定异常处理");
+    }
+
+    @ExceptionHandler(YiJunException.class)
+    @ResponseBody
+    public Result error(YiJunException e){
+        e.printStackTrace();
+        return Result.fail().message(e.getMessage()).code(e.getCode());
+    }
+}
